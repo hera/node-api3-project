18eace59b2aecbacca501f41cf488c265bf8847e
hera <a.heraimenka@gmail.com>
2020-07-08 16:01:30 -0700

    Add logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code away!
+const express = require("express");
+const userRouter = require("./users/userRouter");
+
+const PORT = 8000;
+const ADDRESS = "127.0.0.1";
+
+
+const server = express();
+
+server.use(logger);
+server.use(express.json());
+
+server.use("api/users", userRouter);
+
+server.listen(PORT, ADDRESS, () => console.log("Server is working..."));
+
+function logger(req, res, next) {
+    const currentDate = new Date();
+    console.log(`${req.method} "${req.path}" ${currentDate.toLocaleDateString()} at ${currentDate.toLocaleTimeString()}`);
+    next();
+}
